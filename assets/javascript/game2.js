d2ca26d63102707841f1348712503abbe2038eb6
Kevin Whiteside 1 <kevinwhiteside1@Kevins-MacBook-Pro.local>
2016-07-28 03:55:54 -0400

    perfect

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	var time;
 	var count;
@@ -7,8 +6,10 @@ $(document).ready(function(){
 		{question: "Who killed Jango Fett?", answer: "Mace Windu", guess1: "Yoda", guess2: "Qui-Gon Jinn", guess3: "Darth Sidius", gif: "jango.gif"}, 
 		{question: "Trained Obi-Wan?",answer: "Qui-Gon Jinn", guess1: "Anakin Skywalker", guess2: "Mace Windu", guess3: "Darth Sidius", gif: "qui-gon.gif"},
 		];
-	
-	function resetValues(){
+		
+	startGame();
+
+	function startGame(){
 		workingArray = [
 		{question: "Who defeated Darth Maul?",answer: "Obi-Wan Kenobi", guess1: "Anakin Skywalker", guess2: "Luke Skywalker", guess3: "Darth Maul", gif: "darthmaul.gif"}, 
 		{question: "Who killed Jango Fett?", answer: "Mace Windu", guess1: "Yoda", guess2: "Qui-Gon Jinn", guess3: "Darth Sidius", gif: "jango.gif"}, 
@@ -18,48 +19,62 @@ $(document).ready(function(){
 		correct = 0;
 		incorrect = 0;
 		unAnswered = 0;
-		clearInterval(time);
-		startGame();
-
-	}
-
-
-
-
-	resetValues();
-	
-	function startGame(){
-		
-		// WHY IS THIS ASSIGNING BACKWARDS?????
-		// WHY IS THIS ASSIGNING BACKWARDS?????
-		// WHY IS THIS ASSIGNING BACKWARDS?????
-		
-
 
 		$("#startbutton").show();
 		$("#gamecontainer").hide();
 		$("#answers").hide();
+		$("#replaybutton").click(resetGame);
 		$("#endgame").hide();
+		
 		$("#startbutton").click(function(){
-			$("#startbutton").hide()
-			$("#gamecontainer").show()
-			fillWords()
-			// function(){
-			// 	$("#startbutton").fadeOut("slow", function(){
-			// 		$("#gamecontainer").fadeIn("slow", fillWords);
-			// 	});		
-			// }
-		});
+				$("#startbutton").fadeOut("slow", fillWords);		
+			}
+
+		);
 	};
 
+
 	
+
+
+	function fillWords(){
+		$("#answers").hide();
+
+		//TIMER STUFF
+		secondsLeft = 15;
+		clearInterval(time);
+		$("#timer").html(secondsLeft);
+		time = setInterval(countDown, 1000);
+		// TIMER STUFF END
+		
+		var randomNumber = Math.floor((Math.random() * workingArray.length));
+		
+		randomObject = workingArray[randomNumber];
+		workingArray.splice(randomNumber, 1);
+		
+		$("#questiontext").html(randomObject.question);
+		
+		
+		var guessesArray = [randomObject.answer, randomObject.guess1, randomObject.guess2, randomObject.guess3];
+		for (var i = 1; i < 5; i++) {
+			var randomNumber = Math.floor((Math.random() * guessesArray.length))
+			var randomGuess = guessesArray[randomNumber];
+			$("#b" + i).html(randomGuess);
+			guessesArray.splice(randomNumber, 1);
+		}
+		$("#gamecontainer").fadeIn();
+		$("#buttongroup").fadeIn();
+
+	}
+	//FUNCTION FILLWORDS END
+
 
 	// THIS IS MY TIMER 
 	function countDown(){
 		secondsLeft--;
 		$("#timer").html(secondsLeft);
 		if (secondsLeft < 1) {
-			console.log("TIME");
+			// console.log("TIME");
 			clearInterval(time);
 			$("#questiontext").html("Out of time!");
 			$("#buttongroup").hide();
@@ -80,114 +95,33 @@ $(document).ready(function(){
 				setTimeout(fillWords, 4000);
 			};
 		};
+	};//TIMER END
 	
-		
-
-
-
-
-
-	};
-	//TIMER END
-
-
-
-	// function startFillWords(){
-	// 	$("#startbutton").hide();
-	// 	$("#answertext").hide();
-	// 	setTimeout(fillWords, 3000);
-	// }
-
-
-	function fillWords(){
-		$("#answers").hide();
-		// console.log("correct: " + correct);
-		// console.log("incorrect: " + incorrect);
-		// console.log("unAnswered: " + unAnswered);
-		console.log("YOU SHOULD ONLY SEE THIS ONCE PER QUESTION");
-
-		// SOMEHOW INDEX IS INCREASING BY MORE THAN 1, LIKE EXPONENTIALLY. 
-		// // SOMEHOW INDEX IS INCREASING BY MORE THAN 1, LIKE EXPONENTIALLY. 
-		// console.log("At the beginning of fillwords index is: " + index);
-		
-		// THIS ALL FIRES MORE THAN ONCE// THIS ALL FIRES MORE THAN ONCE// THIS ALL FIRES MORE THAN ONCE
-		// THIS ALL FIRES MORE THAN ONCE
-		// THIS ALL FIRES MORE THAN ONCE
-		
-
-		//TIMER STUFF
-		secondsLeft = 10;
-		clearInterval(time);
-		$("#timer").html(secondsLeft);
-		time = setInterval(countDown, 1000);
-		// TIMER STUFF END
-
-		
-		
-		// GETS A RANDOM OBJECT
-				// I'M GONNA TAKE THEM OUT AS THEY ARE PICKED
-		// FILLS THE QUESTION AND GUESSES HTML
-		// YEAH IT REPEATS RIGHT NOW
-		// NOT THE PROBLEM
-
-		
-		// workingObjectArray = [
-		// {question: "Who defeated Darth Maul?",answer: "Obi-Wan Kenobi", guess1: "Anakin Skywalker", guess2: "Luke Skywalker", guess3: "Darth Maul", gif: "darthmaul.gif"}, 
-		// {question: "Who killed Jango Fett?", answer: "Mace Windu", guess1: "Yoda", guess2: "Qui-Gon Jinn", guess3: "Darth Sidius", gif: "jango.gif"}, 
-		// {question: "Who trained Obi-Wan Kenobi?",answer: "Qui-Gon Jinn", guess1: "Anakin Skywalker", guess2: "Mace Windu", guess3: "Darth Sidius", gif: "qui-gon.gif"},
-		// ];
-		
-		var randomNumber = Math.floor((Math.random() * workingArray.length))
-		
-		randomObject = workingArray[randomNumber];
-		workingArray.splice(randomNumber, 1);
-		
-		$("#questiontext").html(randomObject.question);
-		
-		
-		var guessesArray = [randomObject.answer, randomObject.guess1, randomObject.guess2, randomObject.guess3];
-		for (var i = 1; i < 5; i++) {
-			var randomNumber = Math.floor((Math.random() * guessesArray.length))
-			var randomGuess = guessesArray[randomNumber];
-			$("#b" + i).html(randomGuess);
-			guessesArray.splice(randomNumber, 1);
-		}
-		$("#buttongroup").show()
-		
-	
-
-
-	}
-	//FUNCTION FILLWORDS END
-
 
 	$(".guess").on("click", function(){
 		clearInterval(time);
 		var yourGuess = $(this).html();
+		
 		if(yourGuess == randomObject.answer){
 			$("#questiontext").html("Correct!");
 			correct++
-			// console.log("count: " + count);
-			count++
-			// return correct;
-			
+		
+			count++		
 		}
 		else{
 			$("#questiontext").html("Nope!");			
-			// console.log("count: " + count);
+		
 			count++
 			incorrect++;
-			// return incorrect;
+		
 		};
 		
-
-		console.log("count++ is: " + count);
 		$("#buttongroup").hide();
 		$("#answers").show();
 		$("#answers").html("<h2>The Correct Answer was: " + randomObject.answer + "</h2><br><img src='assets/images/" + randomObject.gif + "'>");
 				
 		if(count >= objectArray.length){
-			// console.log("Finish");
+		
 			setTimeout(finishGame, 4000);
 		}else {
 			// console.log("Continue");
@@ -197,43 +131,34 @@ $(document).ready(function(){
 
 	});
 
-	// function correctAnswer(){
-	// 	correct++;
-	// 	return correct;
-	// }
-	// function incorrectAnswer(){
-	// 	return incorrect++;
-	// 	console.log(incorrect);
-	// }
-	// function unAnswered(){
-	// 	return unAnswered++;
-	
-	
 
-	// if(correct + incorrect + unAnswered > 1){
-	// 			finishGame();
-	// 		}
 
 	function finishGame(){
-		$("#gamecontainer").fadeOut("slow");
-		$("#endgame").fadeIn("slow", function(){
-			play = confirm("PLAY AGAIN?");
-			if (play) {
-			console.log("RESTARTING GAME");
-			resetValues();
-		}
-		else{
-			$("#gamecontainer").fadeOut("slow");
+		$("#gamecontainer").fadeOut("slow", function(){
+			$("#score").html("Your Score: <br>Correct Answers: " + correct + "<br>Wrong Answers: " + incorrect + "<br>Unanswered: " + unAnswered);
 			$("#endgame").fadeIn("slow");
+		});	
+		
 		};
-		});
 
-		
-		
+
+	function resetGame(){
+		workingArray = [
+		{question: "Who defeated Darth Maul?",answer: "Obi-Wan Kenobi", guess1: "Anakin Skywalker", guess2: "Luke Skywalker", guess3: "Darth Maul", gif: "darthmaul.gif"}, 
+		{question: "Who killed Jango Fett?", answer: "Mace Windu", guess1: "Yoda", guess2: "Qui-Gon Jinn", guess3: "Darth Sidius", gif: "jango.gif"}, 
+		{question: "Trained Obi-Wan?",answer: "Qui-Gon Jinn", guess1: "Anakin Skywalker", guess2: "Mace Windu", guess3: "Darth Sidius", gif: "qui-gon.gif"},
+		];
+		count = 0;
+		correct = 0;
+		incorrect = 0;
+		unAnswered = 0;
+		clearInterval(time);
+		// startGame();
+		$("#startbutton").show();
+		$("#gamecontainer").hide();
+		$("#answers").hide();
+		$("#endgame").hide();
+
 	};
-
-
-	
-
 
 });
